b655badf89434993b109d1b042902a4d5fb965ba
Sarah Bueno <sarahbrjakubowski@gmail.com>
2026-07-22 17:34:55 -0300

    Add SQL queries for filtering clients and products
    Added SQL queries to filter clients and products based on specific criteria.

---
+++ consultas/where.sql
@@ -0,0 +1,19 @@
+-- WHERE: Específica um filtro para a consulta que estamos aplicando.
+
+-- selecione todas as colunas da tabela clientes em que o tipo-cliente é igual a 'PF'
+
+SELECT * 
+FROM clientes
+WHERE tipo-cliente = 'PF';
+
+-- selecione o primeiro nome, código do cliente e Estado da tabela clientes em que o Estado é 'PR', 'SP' e 'MG'.
+-- in : Específica uma lista de valores a serem filtrados.
+
+SELECT primeiro_nome, cod_cliente, uf
+FROM clientes
+WHERE uf in ('PR', 'SP', 'MG');
+
+-- Seleciona todas as colunas da tabela produtos em que o preço é maior que R$100.
+SELECT *
+FROM produtos
+WHERE preco > 100;
