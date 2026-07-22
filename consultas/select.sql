7a4cccdb673dd247ee307e8c502530aa2f9d71ba
Sarah Bueno <sarahbrjakubowski@gmail.com>
2026-07-22 17:12:04 -0300

    Create select.sql

---
+++ consultas/select.sql
@@ -0,0 +1,17 @@
+-- SELECT: Utilizado para selecionar dados de uma ou mais colunas de uma tabela.
+
+-- selecione uma coluna da tabela
+SELECT nome_coluna 
+FROM nome_tabela;
+
+-- selecione todas as colunas da tabela
+SELECT * 
+FROM nome_tabela;
+
+-- renomeando uma coluna (as)
+SELECT nome as novo_nome
+FROM nome_tabela;
+
+-- selecione valores distintos de uma coluna
+SELECT distinct nome 
+FROM nome_tabela;
