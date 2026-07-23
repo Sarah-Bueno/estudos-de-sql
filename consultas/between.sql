cf2587fb9359507b82d57f1d2ec9d7de50b2c991
Sarah Bueno <sarahbrjakubowski@gmail.com>
2026-07-23 14:36:39 -0300

    Add SQL queries using BETWEEN for various conditions

--- consultas/between.sql
+++ consultas/between.sql
@@ -7,5 +7,28 @@
 SELECT primeiro_nome, cod_cliente, uf
 FROM clientes
 WHERE  data_cadastro 
-BETWEEN '01/01/2024' 
-AND '01/01/2025';
+BETWEEN '01/01/2024' AND '01/01/2025';
+
+-- selecione a coluna primeiro nome da tabela  clientes em que a coluna id cadastro está entre 1 e 50.
+
+SELECT primeiro_nome
+FROM clientes
+WHERE id_cadastro
+BETWEEN 1 AND 50;
+
+-- selecione todas as colunas da tabela pessoas em que a coluna idade está entre 20 a 35 anos.
+
+SELECT *
+FROM pessoas
+WHERE idade 
+BETWEEN 20 AND 35;
+
+-- selecione as colunas nome e salário da tabela funcionários em que salário está entre R$ 3.0000,00 a R$ 5.000,00 reais.
+
+SELECT nome, salario
+FROM funcionarios
+WHERE salario 
+BETWEEN 3000 AND 5000;
+
+
+
